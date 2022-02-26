3666c319af7deb4279016afc11a78c720665bf8b
charbelllll <87478970+charbelllll@users.noreply.github.com>
2022-02-26 15:00:03 +0200

    fixed-perspective

--- animate.js
+++ animate.js
@@ -210,7 +210,7 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(1, 1, 1);
   camera = new THREE.PerspectiveCamera(
-    75,
+    40,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
